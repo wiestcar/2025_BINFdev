60fd4a5988f487181f188b143d66276b738b81b9
root <root@DESKTOP-N4K79HO.>
2025-01-12 21:59:52 -0500

    Added variable3

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 # Description: Run script which deploys the nextflow workflow
 
 # Input args
-profile=$1 
+profile=$1
 outDir=$2
+# added 3rd variable
+projectID=$3
 
 # housekeeping
 if [[ -z $outDir || -z $profile || -z $projectID ]]; then echo "All variables are required: profile outDir projectID"; exit; fi
