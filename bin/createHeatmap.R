33ff2dd2d53400d75cbb72cef278693003632535
root <root@DESKTOP-N4K79HO.>
2025-01-13 20:16:53 -0500

    Added code for basic and complex heatmap

--- bin/createHeatmap.R
+++ bin/createHeatmap.R
@@ -18,7 +18,7 @@ option_list <- list(
   make_option(c("-g", "--geneFunctions_file"), type="character", default=NULL, metavar="path", help="Gene Functions file."),
   make_option(c("-a", "--annoData_file"), type="character", default=NULL, metavar="path", help="Annotation Data file."),
   make_option(c("-p", "--outprefix"), type="character", default='projectID', metavar="string", help="Output prefix.")
-)
+ )
 
 
 opt_parser <- OptionParser(option_list=option_list)
@@ -73,10 +73,39 @@ annoColors <- list(
 ## Create a basic heatmap##
 ################################################
 ################################################
-
+#define basic colors
+colors <- brewer.pal(9, "Y1OrRd")
+pheatmap(
+   sampleData,
+   clustering_distance_rows="euclidean",
+   clustering_distance_cols="euclidean",
+   clustering_method="ward.D",
+   color=colors
+)
+#save as pdf
+dev.copy(pdf, "basic_heatmap_{projectID}.pdf", width=500, height=300)
+dev.off()   
 
 ################################################
 ################################################
 ## Create a basic heatmap##
 ################################################
 ################################################
+colors <- brewer.pal(9, "Y1OrRd")
+pheatmap(
+   sampleData,
+   annotation_col=annoData,
+   annotation_colors=annoColors,
+   clustering_distance_rows="euclidean",
+   clustering_distance_cols="euclidean",
+   clustering_method="ward.D",
+   color=colors
+   legend_brakes=seq(-1,2,by=1),
+   legen_labels=c("Low","Medium","High"),
+   show_rownames=FALSE,
+   show_colnames=FALSE
+)
+)
+#save as pdf
+dev.copy(pdf, "complex_heatmap_{projectID}.pdf", width=500, height=300)
+dev.off()
